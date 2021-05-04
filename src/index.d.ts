554324ad5915f0fe1cf9748eebf3eb2cf337a8a6
jeffrey.p.gunzelman <jeffrey.gunzelman@phoenix-opsgroup.com>
2021-05-03 20:38:25 -0400

    fixed issues with package

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,4 @@
-declare module "metar-plot"
 export { MetarPlot, metarToSVG, rawMetarToSVG } from "./MetarPlot"
 export { METAR } from "./Metar"
 export { getWeatherSVG } from "./Weather"
+declare module "metar-plot"
