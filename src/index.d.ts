92625fc7ec902e76bd15f454366b608eb617fae4
jeffrey.p.gunzelman <jeffrey.gunzelman@phoenix-opsgroup.com>
2021-11-04 12:00:14 -0400

    🐛  didn't add to index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,6 @@
 export { MetarPlot, metarToSVG, rawMetarToSVG } from "./MetarPlot"
 export { METAR, parseMetar } from "./Metar"
-export { WEATHER, getWeatherSVG } from "./parts/Weather"
+export { WEATHER, getWeatherSVG, getWeatherLegend } from "./parts/Weather"
+export { genWind } from './parts/Wind'
 export { CLOUDS } from './parts/Cloud'
 declare module "metar-plot"
